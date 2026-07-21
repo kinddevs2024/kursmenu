a3dbbe14b3c5e1462f451bfee32f3f4ac1fa2d23
umarbek <kinddevs2024@gmail.com>
2026-07-21 19:42:35 +0500

    Trust Vercel reverse proxy

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,7 @@ const http = require('http');
 const { Server } = require('socket.io');
 
 const app = express();
+app.set('trust proxy', 1);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
